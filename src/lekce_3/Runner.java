3450ef9cba119491ef793fc8614e2aba811858c4
ondre <ondre@192.168.0.87>
2018-12-18 17:40:07 +0100

    zkouška změn

--- src/lekce_3/Runner.java
+++ src/lekce_3/Runner.java
@@ -4,12 +4,12 @@ public class Runner {
 
 	public static void main(String[] args) {
 		Produkt kofila = new Produkt();
-		Produkt mila = new Produkt(13,"Mila",50,"g",12.50f);
+		Produkt milka = new Produkt(13,"Milka",50,"g",12.50f);
 		
 		/*(int id, String name, int volume, String volumeUnit, float price)*/
 		
 		System.out.println(kofila.id);
-		System.out.println(mila.name);
+		System.out.println(milka.name);
 	}
 
 }
